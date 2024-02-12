b60aa4bc9bd584955f1f3d5b1bad6e2abba1e399
Klenovszky Áron <aron.klenovszky@gmail.com>
2024-02-12 23:52:25 +0100

    Connecting room generation

---
+++ Project_Tower/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    private TileManager _TileManagerPrefab;
+
+    public int RoomCounter;
+    private int RoomLeft;
+    // Start is called before the first frame update
+    private int N;
+
+    private int[,] RoomsGrid;
+
+    void Start()
+    {
+        N = RoomCounter / 2;
+        RoomLeft = RoomCounter;
+        int x = Random.Range(0, N);
+        int y = Random.Range(0, N);
+        RoomsGrid = new int[N,N];
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = 0; j < N; j++)
+            {
+                RoomsGrid[i,j] = 0;
+            }
+        }
+        RandomizeRooms(x, y, 2);
+
+        string s = "";
+
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = 0; j < N; j++)
+            {
+                s += RoomsGrid[i,j];
+            }
+            Debug.Log(s);
+            s = "";
+        }
+    }
+
+    private bool RandomizeRooms(int x, int y, int type)
+    {
+        if(x >= 0 && x < N && y >= 0 && y < N && RoomLeft > 0){
+            if(RoomLeft == 1)
+                type = 3;
+            bool newRoom = true;
+            if(RoomsGrid[x,y] == 0)
+            {
+                RoomsGrid[x,y] = type;
+                RoomLeft--;
+                newRoom = false;
+            }
+            bool reValue = false;
+            int bySide = 2;
+            int tryCount = 4;
+            while(bySide > 0 && tryCount > 0){
+                switch(Random.Range(0,4)){
+                    case 0: 
+                        reValue = RandomizeRooms(x-1,y, 1);
+                        break;
+                    case 1: 
+                        reValue = RandomizeRooms(x+1,y, 1);
+                        break;
+                    case 2: 
+                        reValue = RandomizeRooms(x,y-1, 1);
+                        break;
+                    case 3: 
+                        reValue = RandomizeRooms(x,y+1, 1);
+                        break;
+                }
+                tryCount--;
+                if(reValue){
+                    bySide--;
+                    tryCount = 3;
+                    reValue = false;
+                }
+            }
+            return newRoom;
+        }
+        return false;
+    }
+}
