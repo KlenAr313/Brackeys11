bbd284672e9121bbf261139e52355e6891dd5e3f
Klenovszky Áron <aron.klenovszky@gmail.com>
2024-02-13 20:01:27 +0100

    Not working shit

---
+++ Project_Tower/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private RoomManager roomManager;
+    [SerializeField] private int RoomCounter;
+    private int RoomLeft;
+    private int N;
+    private int[,] RoomsGrid;
+    private int CurrentRow;
+    private int CurrentCol;
+
+    void Start()
+    {
+        N = RoomCounter / 2;
+        RoomLeft = RoomCounter;
+        CurrentRow = UnityEngine.Random.Range(0, N);
+        CurrentCol = UnityEngine.Random.Range(0, N);
+        RoomsGrid = new int[N,N];
+        RoomsGrid = new int[N+2,N+2];
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = 0; j < N; j++)
+            {
+                RoomsGrid[i,j] = 0;
+            }
+        }
+        RandomizeRooms(CurrentRow, CurrentCol, 2);
+
+        string s = "";
+
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = 0; j < N; j++)
+            {
+                s += RoomsGrid[i,j];
+            }
+            Debug.Log(s);
+            s = "";
+        }
+
+        bool[] doors = new bool[4];
+        doors[0] = CurrentRow < 0;
+        doors[1] = CurrentCol >= N;
+        doors[2] = CurrentRow >= N;
+        doors[3] = CurrentCol < 0;
+        roomManager.NextRoom(doors);
+
+        OpenDoor(0);
+    }
+
+    private bool RandomizeRooms(int x, int y, int type)
+    {
+        if(x >= 0 && x < N && y >= 0 && y < N && RoomLeft > 0){
+            if(RoomLeft == 1)
+                type = 3;
+            bool newRoom = false;
+            if(RoomsGrid[x,y] == 0)
+            {
+                RoomsGrid[x,y] = type;
+                RoomLeft--;
+                newRoom = true;
+            }
+            bool reValue = false;
+            int bySide = 2;
+            int tryCount = 4;
+            while(bySide > 0 && tryCount > 0){
+                switch(UnityEngine.Random.Range(0,4)){
+                    case 0: 
+                        reValue = RandomizeRooms(x-1,y, 1);
+                        break;
+                    case 1: 
+                        reValue = RandomizeRooms(x+1,y, 1);
+                        break;
+                    case 2: 
+                        reValue = RandomizeRooms(x,y-1, 1);
+                        break;
+                    case 3: 
+                        reValue = RandomizeRooms(x,y+1, 1);
+                        break;
+                }
+                tryCount--;
+                if(reValue){
+                    bySide--;
+                    tryCount = 3;
+                    reValue = false;
+                }
+            }
+            return newRoom;
+        }
+        return false;
+    }
+
+    public bool OpenDoor(int doorI){
+        int NextRow = CurrentRow;
+        int NextCol = CurrentCol;
+        switch (doorI)
+        {
+            case 0:
+                NextRow--;
+                break;
+            case 1:
+                NextCol++;
+                break;
+            case 2:
+                NextRow++;
+                break;
+            case 3:
+                NextCol--;
+                break;
+            default:
+                return false;
+        }
+
+        if(NextCol >=0 && NextCol >= 0 && NextRow < N && NextCol < N && RoomsGrid[NextRow,NextCol] != 0){
+            CurrentRow = NextRow;
+            CurrentCol = NextCol;
+            bool[] doors = new bool[4];
+            doors[0] = CurrentRow < 0;
+            doors[1] = CurrentCol >= N;
+            doors[2] = CurrentRow >= N;
+            doors[3] = CurrentCol < 0;
+            roomManager.NextRoom(doors);
+            return true;
+        }
+        return false;
+    }
+
+}
