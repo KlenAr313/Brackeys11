15f274fbc6f9e1982eb1484fd02665c6b02520e7
IncepT01 <tiszagergo10@gmail.com>
2024-07-14 20:47:42 +0200

    movement progress

--- Project_Tower/Assets/Scripts/Abilities/Move.cs
+++ Project_Tower/Assets/Scripts/Abilities/Move.cs
@@ -10,16 +10,27 @@ public class Move : SpellBase
     public override List<Vector2Int> GetEffectedTiles(int posX, int posY){
 
         List<Vector2Int> coords = new List<Vector2Int>();
+        posX = Player.Instance.currCharacter.PosX;
+        posY = Player.Instance.currCharacter.PosY;
         int moveRange = Player.Instance.currCharacter.moveRange;
-        int offset = moveRange;
 
-        for(int i = -offset; i < offset; i++){
-            if(math.abs(posX - (posX + i)) +  math.abs(posY - (posY + i))< moveRange ){
-                coords.Add(new Vector2Int(posX+i, posY+i));
+        for(int i = -moveRange; i <= moveRange; i++){
+            
+            for(int j = -moveRange; j <= moveRange; j++){
+
+                if(math.abs(posX - (posX + i)) +  math.abs(posY - (posY + j))< moveRange ){
+                    coords.Add(new Vector2Int(posX+i, posY+j));
+                }
+
             }
+
+
+            Debug.Log("Távolság X: " + math.abs(posX - (posX + i)));
+            Debug.Log("Távolság Y: " + math.abs(posY - (posX + i)));
+
         }
 
-        Debug.Log(coords[2]);
+        //Debug.Log(coords[2]);
 
         return coords;
     }
